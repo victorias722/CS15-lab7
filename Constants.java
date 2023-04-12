d2fa50fdad429e9dcc4c59ecbb4a4e74f990eda0
victorias722 <119906318+victorias722@users.noreply.github.com>
2023-04-12 15:07:49 -0400

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,20 @@
+package lab7;
+
+/**
+ * ____                      _              _ _ _     _
+ * |  _ \  ___    _ __   ___ | |_    ___  __| (_) |_  | |
+ * | | | |/ _ \  | '_ \ / _ \| __|  / _ \/ _` | | __| | |
+ * | |_| | (_) | | | | | (_) | |_  |  __/ (_| | | |_  |_|
+ * |____/ \___/  |_| |_|\___/ \__|  \___|\__,_|_|\__| (_)
+ * <p>
+ * This is support code for the lab. You may look at it if you are interested,
+ * but you will not have to change anything to complete your lab.
+ */
+
+public class Constants {
+
+    public static final int PANEL_W = 600;
+    public static final int PANEL_H = 500;
+    public static final int CP_HEIGHT = 40;
+
+}
