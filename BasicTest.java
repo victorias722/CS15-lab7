08f78d3c8e12e368fc6fb872acbd9c8fc878ae66
victorias722 <119906318+victorias722@users.noreply.github.com>
2023-04-12 15:05:13 -0400

    Create BasicTest.java

---
+++ BasicTest.java
@@ -0,0 +1,31 @@
+package lab7;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class BasicTest {
+
+    /**
+     * Example of testing 1 + 1 = 2
+     */
+    @Test
+    public void testOnePlusOne() {
+        assertTrue(1 + 1 == 2);
+    }
+
+    /**
+     * Broken test
+     * TODO: fix this test
+     */
+    @Test
+    public void testTwoTimesThree() {
+        assertTrue(2 * 2 == 6);
+    }
+
+    /**
+     * TODO: Write a test that tests the output of 6 divided by 2
+     * TODO: Follow the test method format above
+     */
+
+}
